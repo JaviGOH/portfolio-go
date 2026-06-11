8c50f83986abc8a38225080545e26abf4884c3e9
JaviGOH <fjgallegosortega@gmail.com>
2026-06-11 20:01:35 +0200

    Setup Java Swing calculator UI with MVC structure and basic layout

---
+++ Projects/java-swing-calculator/src/com/javigoh/calculator/main/Main.java
@@ -0,0 +1,11 @@
+package com.javigoh.calculator.main;
+
+import com.javigoh.calculator.view.CalculatorView;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CalculatorView view = new CalculatorView();
+        view.setVisible(true);
+    }
+}
---
+++ Projects/java-swing-calculator/src/com/javigoh/calculator/view/CalculatorView.java
@@ -0,0 +1,43 @@
+package com.javigoh.calculator.view;
+
+import javax.swing.*;
+import java.awt.GridLayout;
+
+public class CalculatorView extends JFrame {
+
+    private JPanel rootPanel;
+
+    private JTextField Pantalla;
+    private JButton Number7;
+    private JButton Number5;
+    private JButton Number6;
+    private JButton MultiplicationOperation;
+    private JButton Number4;
+    private JButton Number1;
+    private JButton Number8;
+    private JButton Number2;
+    private JButton Number3;
+    private JButton Number9;
+    private JButton DivisionOperation;
+    private JButton SubtractOperation;
+    private JButton Number0;
+    private JButton Point;
+    private JButton AdditionOperation;
+    private JButton Result;
+    private JPanel panelPantalla;
+    private JPanel panelBotones;
+
+    public CalculatorView() {
+        setTitle("Calculadora GO");
+        setContentPane(rootPanel);
+        setContentPane(panelBotones);
+
+        panelBotones.setLayout(new GridLayout(5, 5));
+
+        setSize(600,800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+    }
+}
+
